4d14e0d6ab0e226c3617fd745aec1ed0da584c0c
HediyeSeza <sezahedi13@gmail.com>
2026-08-05 10:48:50 +0330

    feat: improve note creation flow and UI

---
+++ src/components/HomeContent/HomeContent.tsx
@@ -0,0 +1,31 @@
+import TopBar from "../TopBar";
+import SearchSection from "../SearchBar/SearchSection";
+import NotesGrid from "../NotesGrid/NotesGrid";
+
+import type { Note } from "../../types/note";
+
+type HomeContentProps = {
+  notes: Note[];
+  fetchNotes: () => Promise<void>;
+};
+
+const HomeContent = ({
+  notes,
+  fetchNotes,
+}: HomeContentProps) => {
+  return (
+    <>
+      <TopBar
+  fetchNotes={fetchNotes}
+  notesCount={notes.length}/>
+
+      <div className="px-6 py-8 lg:px-10">
+        <SearchSection />
+
+        <NotesGrid notes={notes} />
+      </div>
+    </>
+  );
+};
+
+export default HomeContent;
--- src/components/SearchBar/SortDropdown.tsx
+++ src/components/SearchBar/SortDropdown.tsx
@@ -1,21 +1,58 @@
+import { ChevronDown } from "../../assets/icons";
+
 const SortDropdown = () => {
   return (
-    <select
-      className="
-        h-14
-        rounded-2xl
-        border
-        border-slate-200
-        bg-white
-        px-4
-        dark:border-slate-700
-        dark:bg-slate-900
-      "
-    >
-      <option>Newest</option>
-      <option>Oldest</option>
-      <option>A-Z</option>
-    </select>
+    <div className="relative">
+      <select
+        className="
+          h-14
+          w-full
+
+          appearance-none
+
+          rounded-2xl
+
+          border
+          border-slate-200
+          dark:border-slate-700
+
+          bg-white
+          dark:bg-slate-800
+
+          px-4
+          pr-12
+
+          text-slate-700
+          dark:text-white
+
+          outline-none
+        "
+      >
+        <option>Newest</option>
+        <option>Oldest</option>
+        <option>A-Z</option>
+      </select>
+
+      <img
+        src={ChevronDown}
+        alt="Chevron Down"
+        className="
+          pointer-events-none
+
+          absolute
+          right-4
+          top-1/2
+
+          h-5
+          w-5
+
+          -translate-y-1/2
+
+          opacity-70
+          dark:opacity-100
+        "
+      />
+    </div>
   );
 };
 
