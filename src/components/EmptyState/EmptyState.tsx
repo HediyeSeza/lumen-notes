29caa80a4c02c930cbc8752f8098081a0fde68f3
HediyeSeza <sezahedi13@gmail.com>
2026-08-05 22:02:46 +0330

    feat: implement empty-state

---
+++ src/components/EmptyState/EmptyState.tsx
@@ -0,0 +1,43 @@
+type EmptyStateProps = {
+  title: string;
+  description: string;
+};
+
+const EmptyState = ({
+  title,
+  description,
+}: EmptyStateProps) => {
+  return (
+    <div
+      className="
+        mt-12
+        flex
+        flex-col
+        items-center
+        justify-center
+        rounded-3xl
+        border
+        border-dashed
+        border-slate-300
+        bg-white
+        py-20
+        text-center
+
+        dark:border-slate-700
+        dark:bg-slate-900
+      "
+    >
+      <div className="text-6xl">📝</div>
+
+      <h2 className="mt-6 text-2xl font-bold text-slate-900 dark:text-white">
+        {title}
+      </h2>
+
+      <p className="mt-2 max-w-md text-slate-500 dark:text-slate-400">
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default EmptyState;
